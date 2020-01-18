315b6c1689531cff3cc15237c4ecccc0abf75abe
mannka <kmann1511@gmail.com>
2020-01-18 15:28:12 -0700

    Update form.js

--- form.js
+++ form.js
@@ -231,6 +231,15 @@ submitButton.addEventListener("click", function(e) {
 
 var placeSearch, autocomplete;
 
+// var componentForm = {
+//   // street_number: "short_name",
+//   // route: "long_name"
+//   // locality: "long_name",
+//   // administrative_area_level_1: "short_name",
+//   // country: "long_name",
+//   // postal_code: "short_name"
+// };
+
 function initAutocomplete() {
   // Create the autocomplete object, restricting the search predictions to
   // geographical location types.
@@ -252,20 +261,20 @@ function fillInAddress() {
   // Get the place details from the autocomplete object.
   var place = autocomplete.getPlace();
 
-  for (var component in componentForm) {
-    document.getElementById(component).value = "";
-    document.getElementById(component).disabled = false;
-  }
+  // for (var component in componentForm) {
+  //   document.getElementById(component).value = "";
+  //   document.getElementById(component).disabled = false;
+  // }
 
   // Get each component of the address from the place details,
   // and then fill-in the corresponding field on the form.
-  for (var i = 0; i < place.address_components.length; i++) {
-    var addressType = place.address_components[i].types[0];
-    if (componentForm[addressType]) {
-      var val = place.address_components[i][componentForm[addressType]];
-      document.getElementById(addressType).value = val;
-    }
-  }
+  // for (var i = 0; i < place.address_components.length; i++) {
+  //   var addressType = place.address_components[i].types[0];
+  //   if (componentForm[addressType]) {
+  //     var val = place.address_components[i][componentForm[addressType]];
+  //     document.getElementById(addressType).value = val;
+  //   }
+  // }
 
   document.getElementById("autocomplete").value =
     place.address_components[0]["long_name"] +
@@ -313,3 +322,5 @@ function geolocate() {
 }
 
 google.maps.event.addDomListener(window, "load", initAutocomplete);
+
+console.log("loaded");
